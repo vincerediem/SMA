bdac5defada45ff673cb9ee314ec9f1dc5fd1509
Keith <kopenshaw@me.com>
2023-06-26 14:49:50 -0700

    call stock list funciton from bf

---
+++ SMAv1.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd
+
+import sys
+sys.path.append(r"C:\Users\kopen\OneDrive\Desktop\Code\Algo Trading\Base Functions")
+
+import BaseFunctionv1 as bf
+
+ALPHA_VANTAGE_KEY = 'Y84MMO9G796B0PNO'
+
+stock_list = bf.stock_list()
+
