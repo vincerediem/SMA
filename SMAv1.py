7fa176164309c9898f481c6d668aa66efda5e7d7
Keith <kopenshaw@me.com>
2023-06-30 15:10:25 -0700

    call stock list function

--- SMAv1.py
+++ SMAv1.py
@@ -10,3 +10,8 @@ ALPHA_VANTAGE_KEY = 'Y84MMO9G796B0PNO'
 
 stock_list = bf.stock_list()
 
+def sma(stock_list):
+    url = 'https://www.alphavantage.co/query?function=SMA&symbol=IBM&interval=weekly&time_period=10&series_type=open&apikey=demo'
+    r = requests.get(url)
+    data = r.json()
+    
